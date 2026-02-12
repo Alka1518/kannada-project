4fb665e75f09ca686ffece2494a840a146765269
Alka1518 <alkasonialkasoni3@gmail.com>
2026-02-12 11:09:54 +0530

    Fixed port for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,30 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// require("dotenv").config();
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use("/api/products", require("./routes/productRoutes"));
+
+// // DB Connection
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("✅ MongoDB Connected");
+//     app.listen(5000, () =>
+//       console.log("🚀 Server running on http://localhost:5000")
+//     );
+//   })
+//   .catch((err) => console.log(err));
+
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -5,20 +32,21 @@ require("dotenv").config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/products", require("./routes/productRoutes"));
 
-// DB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB Connected");
-    app.listen(5000, () =>
-      console.log("🚀 Server running on http://localhost:5000")
+
+    // Ye line sabse zaroori hai Render ke liye
+    const PORT = process.env.PORT || 10000; 
+
+    app.listen(PORT, () =>
+      console.log(`🚀 Server running on port ${PORT}`)
     );
   })
   .catch((err) => console.log(err));
